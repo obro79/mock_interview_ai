03e7b176a8641da19cf8da19ca00504e255e483e
owenfisher <owenfisher46@gmail.com>
2025-04-28 09:22:52 -0700

    complete and bug fixed

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -5,9 +5,13 @@ import Image from 'next/image';
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
 import DisplayTechIcons from "@/components/DisplayTechIcons";
+import {getFeedbackByInterviewId} from "@/lib/actions/general.action";
 
-const InterviewCard = ({interviewId, userId, role, type, techstack,createdAt }: InterviewCardProps) => {
-    const feedback = null as Feedback | null;
+const InterviewCard = async ({interviewId, userId, role, type, techstack,createdAt }: InterviewCardProps) => {
+    const feedback = userId && interviewId
+        ? await getFeedbackByInterviewId({interviewId, userId})
+    : null;
+
     const normalizedType = /mix/gi.test(type)? 'Mixed' : type;
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY');
     return (
